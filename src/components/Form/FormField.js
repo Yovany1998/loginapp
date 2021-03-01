23755e36b366af04b1f39fcb0b02d5c26396f8d6
Yovany <geovanyher98@gmail.com>
2021-02-28 18:59:18 -0600

    Correcciones

--- src/components/Form/FormField.js
+++ src/components/Form/FormField.js
@@ -1,8 +1,7 @@
 function FormField({caption, type, id, name, onChange ,value}) {
     
     const inputStyle =[
-        "my-2","rounded-lg" ,"p-2", " w-full","rounded-4", "border", "border-gray-400"
-    zz]
+        "my-2","rounded-lg" ,"p-2", " w-full","rounded-4", "border", "border-gray-400"]
     return (
         <section>
             <input className={inputStyle.join(" ")} placeholder={caption} type={type} id={id} name={name} onChange={onChange} value={value}/>
